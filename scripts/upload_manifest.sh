fd8ecbdee933df36998d04534fa2ba80a166b8e0
tamasveiland <59760494+tamasveiland@users.noreply.github.com>
2020-06-05 16:53:10 +0200

    Update upload_manifest.sh

--- scripts/upload_manifest.sh
+++ scripts/upload_manifest.sh
@@ -4,9 +4,9 @@ app=$(o365 teams app list -o json | jq '.[] | select(.externalId == "'"$APPID"'"
 
 if [ -z "$app" ]; then
   # install app
-  o365 teams app publish -p "./package/TeamsApp.zip"
+  o365 teams app publish -p "./package/second-demo-app.zip.zip"
 else
   # update app
   appId=$(echo $app | jq '.id')
-  o365 teams app update -i $appId -p "./package/TeamsApp.zip"
+  o365 teams app update -i $appId -p "./package/second-demo-app.zip.zip"
 fi
