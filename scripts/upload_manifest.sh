7131ba07b064e1250d7f94ebe50f2f95c5fd0f26
Tamas Veiland <tamas.veiland@microsoft.com>
2020-06-05 16:51:58 +0200

    upload_manifest.sh added.

---
+++ scripts/upload_manifest.sh
@@ -0,0 +1,12 @@
+# o365 login -t password -u $(username) -p $(password)
+
+app=$(o365 teams app list -o json | jq '.[] | select(.externalId == "'"$APPID"'")')
+
+if [ -z "$app" ]; then
+  # install app
+  o365 teams app publish -p "./package/TeamsApp.zip"
+else
+  # update app
+  appId=$(echo $app | jq '.id')
+  o365 teams app update -i $appId -p "./package/TeamsApp.zip"
+fi
